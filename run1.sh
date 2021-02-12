10678b857f82a5802e17fd946e8ab9ef297a13d2
Abhinav7076 <abhinav.pratapsingh555@gmail.com>
2021-02-12 19:36:37 +0530

    lab4

--- run1.sh
+++ run1.sh
@@ -1,3 +1,3 @@
-cp schedule.c /usr/src/minix/servers/sched/;
+cp system.c /usr/src/minix/kernel/;
 cd /usr/src/;
 make build MKUPDATE=yes >log.txt 2>log.txt
--- run2.sh
+++ run2.sh
@@ -1,3 +1,3 @@
-cp system.c /usr/src/minix/kernel/;
+cp schedule.c /usr/src/minix/servers/sched/;
 cd /usr/src/;
 make build MKUPDATE=yes >log.txt 2>log.txt
